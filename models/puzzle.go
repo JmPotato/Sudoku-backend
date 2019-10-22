8f3305ff4af672e732d91d3ae13a95cb1132f966
JmPotato <ghzpotato@gmail.com>
2019-10-22 08:04:43 +0800

    Update puzzle's struct

--- models/puzzle.go
+++ models/puzzle.go
@@ -6,12 +6,12 @@ import (
 
 // Puzzle model
 type Puzzle struct {
-	PID         uint32    `gorm:"primary_key" json:"pid"`
-	Type        uint8     `json:"type"`
-	Content     string    `json:"content"`
-	Descriptor  string    `json:"descriptor"`
-	level       uint8     `json:"level"`
-	Submited    uint32    `json:"submited"`
-	Passed      uint32    `json:"passed"`
-	CreatedTime time.Time `json:"created_time"`
+	PID         uint32    `gorm:"column:pid;primary_key" json:"pid"`
+	Type        uint8     `gorm:"column:type" json:"type"`
+	Content     string    `gorm:"column:content" json:"content"`
+	Descriptor  string    `gorm:"column:descriptor" json:"descriptor"`
+	Level       uint8     `gorm:"column:level" json:"level"`
+	Submited    uint32    `gorm:"column:submited" json:"submited"`
+	Passed      uint32    `gorm:"column:passed" json:"passed"`
+	CreatedTime time.Time `gorm:"column:created_time" json:"created_time"`
 }
